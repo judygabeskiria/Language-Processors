53b4d748baac0c1437f17fc8241c72da80fe8522
judygabeskiria <gabeskiriai@student.mville.edu>
2017-09-14 20:27:47 -0400

    Add files via upload

---
+++ main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  prolog3.pl
+//
+//  Created by ivditi on 2/27/17.
+//  Copyright © 2017 ivditi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
